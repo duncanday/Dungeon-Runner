891490fed07b60bc7f320251681804ad83bc6624
Duncan Day <duncanday94@gmail.com>
2021-09-25 14:00:34 -0700

    Set foundation for Charactermancer
    comments are included for Charactermancer qualities and their use cases.
    a default scene is set up with 5 cubes on orthographic view; this is to demonstrate how to visually think of the AREA_OF_EFFECT scalar.

--- Assets/Charactermancer_t.cs
+++ Assets/Charactermancer_t.cs
@@ -9,7 +9,7 @@ public class Charactermancer_t : MonoBehaviour
 }
 
 /* list of Charactermancer qualities: 
- * BASIC ATTACK (BOOLEAN)
+ * ATTACK (BOOLEAN)
  * SPELL (BOOLEAN)
  * ~ THE ABOVE SERVES AS A FOUNDATION FOR "ABILITIES" ~
  * 
@@ -19,14 +19,14 @@ public class Charactermancer_t : MonoBehaviour
  * 
  * ~ THE ABOVE SERVES AS TRAITS WHICH CHANGE THE INNATE NATURE OF A GIVEN ABILITY OR OTHERWISE ~
  * 
+ * CAST DELAY (FLOAT)
  * CAST DURATION (FLOAT)
  * TARGET SPECIFIER (OBJECT)
  * VALID TARGET QUANTITIES (INTEGER)
  * RADIUS [AREA] OF EFFECT (CALCULATED FLOAT)
  * RESOURCE USED (STRING)
  * COMBO PIECE (BOOLEAN)
- * CHANNELED (BOOLEAN)
- * MULTICHANNELED (BOOLEAN)
+ * ALLOW MULTICAST
  * ~ THE ABOVE SERVES AS DESCRIPTORS THAT CAN BE MODIFIED FOR ABILITIES OR OTHERWISE ~
  * 
  */
@@ -42,7 +42,9 @@ public class Charactermancer
 
     // detail the above traits and elements in serializable fields to enable hot reloading and dynamic iterative design.
 
-    // default bools are set to false.
+    // default bools are initialized as false.
+    // default numerical primitives are initialized as 0, 0.0, or otherwise.
+
     /* 
      * 
      * 
@@ -59,10 +61,59 @@ public class Charactermancer
      * 
      * */
 
-    private bool IS_BASICATTACK; // is this skill quality considered an attack? attacks count towards combo counters.
-    private bool IS_SPELL; // is this skill quality considered a spell? spells do not count towards combo counters.
-    private bool IS_PASSIVE; // is this
-    private bool IS_MODIFIER;
-    private bool IS_TRANSFORMATIVE;
+    private bool IS_ATTACK; // is this skill quality considered an attack? 
+                            // this quality catalyzes combat and "manual" activation; if the skill does not have this designation, it cannot do damage.
+
+    private bool IS_SPELL; // is this skill quality considered a spell? 
+                           // this quality enables other skill methods via a read-only function. (if spell or spell is cast, then...)
+
+    private bool IS_PASSIVE; // is this skill quality considered passive in nature? 
+                             // this quality enables additional effects to be designated without requiring activation via the ATTACK or SPELL designations.
+
+    private bool IS_MODIFIER; // is this skill quality considered a modifier? 
+                              // this quality enables the alteration of attributes associated with characters.
+
+    private bool IS_TRANSFORMATIVE; // is this skill quality considered transformative? 
+                                    // this quality enables the skill to be temporarily replaced by ATTACK or SPELL activations.
+
+    private float CAST_DELAY; // what is the duration of the delay before casting this skill occurs? 
+                              // by design, the delay must decrement to 0 before the skill occurs, or any other skill can be cast.
+
+    private float CAST_DURATION; // what is the duration of the skill's cast? 
+                                 // naturally, as n ~> +inf, duration increases to +inf. computationally, this value must be capped at 240.0 seconds for game design purposes.
+                                 // a value of 0.0f means that the cast is instantaneous after delay has been decremented to 0.
+                                 // a value of exactly -1.0f will be translated to read "infinitely long." 
+                                 // "infinitely long" means that the skill will always be activated and refreshed by the computer's framerate.
+
+    private bool IS_TARGETED; // is this skill a targeted ability? 
+                              // in FPS terms, think of how Titanfall's Smart Pistol doesn't require aiming (except by having the target within FOV).
+
+    private GameObject TARGETS_SPECIFIED; // what are the GameObject references that the skill takes into account? 
+                                          // > Targeted skill: create an abstraction container (when the skill is loaded on the rack.
+                                          // Read the specified GameObject(s) applied by the targeting method (such as selecting via cursor click) and apply game methods.
+                                          // > Untargeted skill: create an abstraction container (when the skill is loaded on the rack).
+                                          // Read all specified GameObject(s) within the AREA OF EFFECT. Apply the appropriate and valid game methods.
+
+    private readonly int VALID_TARGET_QUANTITY; // how many targets within the TARGETS_SPECIFIED container were considered "valid targets?" Debugging variable.
+
+
+    private float AREA_OF_EFFECT; // this is a computational query. the current solution seems computationally heavy and is a rough draft.
+                                  // four Vector2's can be determined from the skill's origin point, and from the "conclusion" target of the cast.
+                                  // these vectors are normalized and then multiplied by this bespoke scalar variable.
+                                  // furthermore, methods that employ AREA_OF_EFFECT create a bounded shape defined by a composite Bezier curve utilizing those 4 points.
+                                  // it can otherwise use simpler shaping methods (circular, square, rectangular, triangular) to use.
+                                  // AREA_OF_EFFECT will multiply the finalized shape (which is not 3-dimensional in this case) to expand its size.
+
+    private string RESOURCE_USED; // what is the type of resource being used for this skill? a custom resource can be created and designated.
+                                  // if no resource is designated, the skill won't require it.
+                                  // this can be referenced via an enum as an alternative.
+
+    private bool IS_COMBO; // the game will be using an in-game combo counting tracker. the number can be used for various numerical method purposes.
+                           // if the skill specifies that this is a combo, any time an designation trigger occurs (attack hit/spellcast success), it adds to the counter.
+
+    private bool ALLOW_MULTICAST; // a multicast skill/ability overrides the CAST_DELAY restrictions.
+                                  // that is, a skill with this quality can cast during the CAST_DELAY restriction period.
+                                  // this is useful for skills that are toggle on/off.
+
     
 }
